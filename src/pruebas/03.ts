a971ccdba71fb749d34be39199a9f5afdf835c48
JChicomeSan <57263663+ChicomeMX7@users.noreply.github.com>
2023-06-09 16:00:18 -0600

    ci: [code:update] main - 2023-06-09 16:00:18

---
+++ src/pruebas/03.ts
@@ -0,0 +1,10 @@
+/* 
+queremos saber cuantos 0 y 1 hay en este string
+
+*/
+const sample = "011010101011110101000111010100010011111110101001010100";
+
+export function cuantaNumeros(param) {
+    
+}
+
---
+++ temas.pl
@@ -0,0 +1,81 @@
+○ controlador de versiones (git ^_^ )
+    • github
+    • ssh
+    • pull
+    • fetch
+    • merge
+    • rebase
+    • checkout
+
+○ data types
+    • primitivos bool|number|string|null|undefined
+    • non-primitivos objects|arrays|functions
+
+○ declarations and properties
+    • scope
+    • types of variables let|const|var
+
+○ array methods
+    • filter
+    • map
+    • forEach
+    • splice
+    • pop/unshift
+    • push/shift
+
+○ key word "this"|"contructors"|"super"
+    • objects
+    • functions
+    • clases
+
+○ loops | data flow
+    • if
+    • for loop
+    • for in
+    • for of
+    • while
+
+○ operands
+    • < | <=
+    • > | >=
+    • !=
+    • ==
+    • ===
+    • && 
+    • ||
+    • %
+
+○ Math
+    • abs
+    • pow
+    • floor
+    • ceil
+    • round
+    • random
+
+○ components
+    • props
+    • destructuring
+    • HOC
+    • ref
+
+○ hooks
+    • useState
+    • useEffect
+    • useCallback
+    • useMemo
+    • useContext
+    • useRef
+
+○ context
+    • Providers
+    • Consumers
+
+○ unit test
+    • jest/vitest
+    • testing library
+    • mock
+    • snapshots
+
+○ custom hooks
+    • useCustomHook
